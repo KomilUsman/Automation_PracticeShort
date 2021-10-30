5941431c7fec99c174e6d0745b2b08df2e0df837
Komil Usmonov <87507641+KomilUsman@users.noreply.github.com>
2021-10-29 21:06:57 -0400

    Update WebDriverFactory.java

--- src/test/java/com/cybertek/utility/WebDriverFactory.java
+++ src/test/java/com/cybertek/utility/WebDriverFactory.java
@@ -5,33 +5,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 public class WebDriverFactory {
 
-    public static WebDriver get(String browserName) {
-        WebDriver driver;
+    public static WebDriver get(String browserName){
 
-        switch (browserName.toLowerCase()) {
+        WebDriver driver ;
 
-            case "chrome":
+        switch (browserName.toLowerCase() ){
+
+            case "chrome" :
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
-
-            case "firefox":
+            case "firefox" :
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
-
+                break;
             default:
-                System.out.println("Unknown browser type " + browserName);
-                driver = null;
+                System.out.println("UNKNOWN BROWSER TYPE " + browserName);
+                driver = null ;
         }
+
         driver.manage().window().maximize();
 
-
-        return null;
-
+        return driver ;
     }
-
 
 }
